5b5a47d8461f7f38fe10404e84ad0d9b15a1c06e
Nestor Martinez <nesturron@gmail.com>
2023-06-11 23:49:15 +0200

    Fixed back-end for 'buddies you trained with' and some minor front-end bugs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,31 @@ class PagesController < ApplicationController
   def home
     @upcoming_events = Event.sort_by_upcoming_dates
     @events_later_today = Event.sort_by_later_today
+    if user_signed_in?
+      @buddies = list_all_uniq_buddies
+    end
+    # @user_age = user_age
   end
 
   def components
+  end
+
+  private
+
+  # def user_age
+  #   DateTime.now.year - user.date_of_birth.year
+  # end
+
+  def list_all_uniq_buddies
+    buddies = []
+    current_user.bookings.each do |booking|
+      buddies << booking.event.event_creator
+    end
+    current_user.events.each do |event|
+      event.bookings.each do |booking|
+        buddies << booking.user
+      end
+    end
+    buddies.uniq
   end
 end
