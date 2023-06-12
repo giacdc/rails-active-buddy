638df1a7d8d7039a338128ac2966c156517d8c9c
Nestor Martinez <nesturron@gmail.com>
2023-06-12 12:46:54 +0200

    Prepared the seed to be easier to modify

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+puts ""
 puts "Cleaning database..."
+
 Feeling.destroy_all
 Booking.destroy_all
 PreferredSport.destroy_all
@@ -16,8 +18,16 @@ SportCategory.destroy_all
 User.destroy_all
 
 puts "Swept and cleansed!"
+puts ""
+puts ""
 
-# 4 USER MODEL
+###################################
+#                                 #
+#                                 #
+#              USERS              #
+#                                 #
+#                                 #
+###################################
 
 puts "Creating users..."
 puts ".........................."
@@ -61,8 +71,16 @@ louisa = User.create!(
 )
 
 puts "Users created!"
+puts ""
+puts ""
 
-# 4 SPORTS CATEGORY
+###################################
+#                                 #
+#                                 #
+#         SPORTS CATEGORIES       #
+#                                 #
+#                                 #
+###################################
 
 puts "Creating sports categories..."
 puts ".........................."
@@ -84,12 +102,19 @@ racketsport = SportCategory.create!(
 )
 
 puts "Sport Categories created!"
+puts ""
+puts ""
 
+###################################
+#                                 #
+#                                 #
+#             SPORTS              #
+#                                 #
+#                                 #
+###################################
 
 puts "Creating sports..."
 puts ".........................."
-
-# 7 SPORTS
 
 kitesurf = Sport.create!(
   name: "Kite Surf",
@@ -133,17 +158,27 @@ salsa = Sport.create!(
   sport_category: artisticsport
 )
 
-# 7 EVENTS
+puts "Sports created!"
+puts ""
+puts ""
+
+###################################
+#                                 #
+#                                 #
+#             EVENTS              #
+#                                 #
+#                                 #
+###################################
 
 puts "Creating events..."
 puts ".........................."
 
-Event.create!(
+event1 = Event.new(
   title: "Kite Surf Chill Session",
   description: "Feel the exhilarating breeze and embrace the thrill of the ocean
   as you embark on a mesmerizing kite surf chill session.",
-  start_date: DateTime.new(2023,6,15,10,0,0),
-  end_date: DateTime.new(2023,6,17,13,0,0),
+  start_date: DateTime.new(2023, 6, 15, 10, 0, 0),
+  end_date: DateTime.new(2023, 6, 17, 13, 0, 0),
   cost: 20,
   max_participants: 10,
   latitude: "",
@@ -152,15 +187,17 @@ Event.create!(
   is_indoor: false,
   sport: kitesurf,
   event_creator: louisa
-).save(validate: false)
+)
 
-event_1 = Event.create!(
+event1.save(validate: false)
+
+event2 = Event.new(
   title: "Sunrise Yoga Meditation",
   description: "Awaken your senses and greet the day with a soul-nourishing Sunrise Yoga Session.
   Breathe in the crisp morning air as you flow through gentle movements, guided by the soft hues
   of the rising sun. ",
-  start_date: DateTime.new(2023,6,10,6,0,0),
-  end_date: DateTime.new(2023,6,10,7,30,0),
+  start_date: DateTime.new(2023, 6, 10, 6, 0, 0),
+  end_date: DateTime.new(2023, 6, 10, 7, 30, 0),
   cost: 20,
   max_participants: 10,
   latitude: "",
@@ -171,13 +208,15 @@ event_1 = Event.create!(
   event_creator: louisa
 )
 
-event_2 = Event.create!(
+event2.save(validate: false)
+
+event3 = Event.create!(
   title: "Discover the art of archery!",
   description: "Discover the art of concentration and control, as you hone your aim and find your inner archer.
   Experience the exhilarating rush of hitting the bullseye for the very first time, and let the joy of this ancient
   sport ignite a newfound passion within you.",
-  start_date: DateTime.new(2023,7,10,13,0,0),
-  end_date: DateTime.new(2023,7,10,15,0,0),
+  start_date: DateTime.new(2023, 7, 10, 13, 0, 0),
+  end_date: DateTime.new(2023, 7, 10, 15, 0, 0),
   cost: 5,
   max_participants: 30,
   latitude: "",
@@ -188,13 +227,13 @@ event_2 = Event.create!(
   event_creator: jacob
 )
 
-event_3 = Event.create!(
+event4 = Event.create!(
   title: "Ping-pong at the park",
   description: "Engage in friendly competition, surrounded by the beauty of nature.
   Laugh, cheer, and enjoy the energetic atmosphere as you showcase your skills and
   create unforgettable moments with friends or fellow players.",
-  start_date: DateTime.new(2023,7,12,16,30,0),
-  end_date: DateTime.new(2023,7,12,18,0,0),
+  start_date: DateTime.new(2023, 7, 12, 16, 30, 0),
+  end_date: DateTime.new(2023, 7, 12, 18, 0, 0),
   cost: 0,
   max_participants: 2,
   latitude: "",
@@ -205,13 +244,13 @@ event_3 = Event.create!(
   event_creator: leo
 )
 
-event_4 = Event.create!(
+event5 = Event.create!(
   title: "A step further",
   description: "Embark on an invigorating trail session in the nature with your buddy.
   Feel the rhythm of your synchronized strides as you explore new paths together.
   Encouragement fills the air, spurring you on to reach new heights.",
-  start_date: DateTime.new(2023,7,12,7,30,0),
-  end_date: DateTime.new(2023,7,12,12,30,0),
+  start_date: DateTime.new(2023, 7, 12, 7, 30, 0),
+  end_date: DateTime.new(2023, 7, 12, 12, 30, 0),
   cost: 0,
   max_participants: 2,
   latitude: "",
@@ -222,13 +261,13 @@ event_4 = Event.create!(
   event_creator: sarah
 )
 
-event_5 = Event.create!(
+event6 = Event.create!(
   title: "Energetic beach volley session!",
   description: "Dive into beach volleyball fun! Communication flows effortlessly through unspoken signals
   and instinctive teamwork. With each powerful serve and skillful spike, you feel the adrenaline surge through
   your veins.",
-  start_date: DateTime.new(2023,6,20,10,0,0),
-  end_date: DateTime.new(2023,6,20,14,0,0),
+  start_date: DateTime.new(2023, 6, 20, 10, 0, 0),
+  end_date: DateTime.new(2023, 6, 20, 14, 0, 0),
   cost: 0,
   max_participants: 2,
   latitude: "",
@@ -239,13 +278,13 @@ event_5 = Event.create!(
   event_creator: louisa
 )
 
-event_6 = Event.new(
+event7 = Event.new(
   title: "Vibrate on the sound of salsa!",
   description: "Let the sultry movements, intricate footwork, and sensual spins transport
    you to a realm of pure euphoria. With every step, you unleash your inner expression,
    releasing inhibitions and embracing the freedom that salsa brings.",
-  start_date: DateTime.new(2023,6,6,19,0,0),
-  end_date: DateTime.new(2023,6,6,20,30,0),
+  start_date: DateTime.new(2023, 6, 6, 19, 0, 0),
+  end_date: DateTime.new(2023, 6, 6, 20, 30, 0),
   cost: 0,
   max_participants: 2,
   latitude: "",
@@ -254,15 +293,17 @@ event_6 = Event.new(
   is_indoor: true,
   sport: salsa,
   event_creator: sarah
-).save(validate: false)
+)
 
-event_7 = Event.create!(
+event7.save(validate: false)
+
+event8 = Event.new(
   title: "Evening beach volley session!",
   description: "Dive into beach volleyball fun! Communication flows effortlessly through unspoken signals
   and instinctive teamwork. With each powerful serve and skillful spike, you feel the adrenaline surge through
   your veins.",
-  start_date: DateTime.new(2023,6,9,18,30,0),
-  end_date: DateTime.new(2023,6,9,20,30,0),
+  start_date: DateTime.new(2023, 6, 9, 18, 30, 0),
+  end_date: DateTime.new(2023, 6, 9, 20, 30, 0),
   cost: 0,
   max_participants: 8,
   latitude: "",
@@ -273,7 +314,9 @@ event_7 = Event.create!(
   event_creator: louisa
 )
 
-event_8 = Event.create!(
+event8.save(validate: false)
+
+event9 = Event.new(
   title: "Ping-pong at Le Wagon",
   description: "Engage in friendly competition, surrounded by laugh and cheer. Enjoy the energetic atmosphere as you showcase your skills and create unforgettable moments with friends or fellow players.",
   start_date: DateTime.new(2023, 6, 9, 19, 30, 0),
@@ -288,7 +331,20 @@ event_8 = Event.create!(
   event_creator: leo
 )
 
-# 3 PREFERRED SPORT PER USER
+event9.save(validate: false)
+
+puts "Events created!"
+puts ""
+puts ""
+
+
+###################################
+#                                 #
+#                                 #
+#    PREFERRED SPORT PER USER     #
+#                                 #
+#                                 #
+###################################
 
 puts "Creating preferred sport per user..."
 puts ".........................."
@@ -366,129 +422,155 @@ PreferredSport.create!(
 )
 
 puts "Created Preferred Sports!"
+puts ""
+puts ""
 
-# 4 BOOKING EVENTS
+###################################
+#                                 #
+#                                 #
+#            BOOKINGS             #
+#                                 #
+#                                 #
+###################################
 
 puts "Creating bookings..."
 puts ".........................."
 
-booking_1 = Booking.create!(
+booking1 = Booking.create!(
   user: leo,
-  event: event_1,
+  event: event1,
   is_accepted: true
 )
 
-booking_2 = Booking.create!(
+booking2 = Booking.create!(
   user: jacob,
-  event: event_2,
+  event: event2,
   is_accepted: true
 )
 
-booking_3 = Booking.create!(
+booking3 = Booking.create!(
   user: sarah,
-  event: event_3,
+  event: event3,
   is_accepted: true
 )
 
-booking_4 = Booking.create!(
+booking4 = Booking.create!(
   user: louisa,
-  event: event_3,
+  event: event3,
   is_accepted: false
 )
 
-booking_5 = Booking.create!(
+booking5 = Booking.create!(
   user: leo,
-  event: event_1,
+  event: event1,
   is_accepted: true
 )
 
-booking_6 = Booking.create!(
+booking6 = Booking.create!(
   user: jacob,
-  event: event_2,
+  event: event2,
   is_accepted: true
 )
 
-booking_7 = Booking.create!(
+booking7 = Booking.create!(
   user: sarah,
-  event: event_3,
+  event: event3,
   is_accepted: true
 )
 
-booking_8 = Booking.create!(
+booking8 = Booking.create!(
   user: louisa,
-  event: event_3,
+  event: event3,
   is_accepted: false
 )
 
-booking_9 = Booking.create!(
+booking9 = Booking.create!(
   user: leo,
-  event: event_1,
+  event: event1,
   is_accepted: true
 )
 
-booking_10 = Booking.create!(
+booking10 = Booking.create!(
   user: jacob,
-  event: event_2,
+  event: event2,
   is_accepted: true
 )
 
-booking_11 = Booking.create!(
+booking11 = Booking.create!(
   user: sarah,
-  event: event_3,
+  event: event3,
   is_accepted: true
 )
 
-booking_12 = Booking.create!(
+booking12 = Booking.create!(
   user: louisa,
-  event: event_3,
+  event: event3,
   is_accepted: false
 )
 
-booking_13 = Booking.create!(
+booking13 = Booking.create!(
   user: leo,
-  event: event_1,
+  event: event1,
   is_accepted: true
 )
 
-booking_14 = Booking.create!(
+booking14 = Booking.create!(
   user: jacob,
-  event: event_2,
+  event: event2,
   is_accepted: true
 )
 
-booking_15 = Booking.create!(
+booking15 = Booking.create!(
   user: sarah,
-  event: event_3,
+  event: event3,
   is_accepted: true
 )
 
-booking_16 = Booking.create!(
+booking16 = Booking.create!(
   user: louisa,
-  event: event_3,
+  event: event3,
   is_accepted: false
 )
 
 puts "Created Bookings!"
+puts ""
+puts ""
 
-# 4 Feeling
+###################################
+#                                 #
+#                                 #
+#             FEELINGS            #
+#                                 #
+#                                 #
+###################################
+
+puts "Creating feelings..."
+puts ".........................."
 
 Feeling.create!(
   user: jacob,
-  booking: booking_2,
+  booking: booking2,
   feeling: 5,
   comment: "Such a nice experience! Amazing team-work!"
 )
 
 Feeling.create!(
   user: sarah,
-  booking: booking_3,
+  booking: booking3,
   feeling: 4,
   comment: "Quick booking confirmation and lovely first time yoga by the beach!"
 )
 
 Feeling.create!(
   user: louisa,
-  booking: booking_4,
+  booking: booking4,
   feeling: 4,
   comment: "Between yoga and meditation, very relaxing!"
 )
+
+puts "Created Feelings!"
+puts ""
+puts ""
+
+puts "Finished generating a nice seed!"
+puts ""
