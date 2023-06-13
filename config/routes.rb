a26cdeb4c4a3817a53d195ef2a5b59282477080a
Laure Descouts <lauredscts@gmail.com>
2023-06-13 17:10:04 +0200

    user profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   resources :events do
     resources :bookings, only: %i[create]
   end
-
+  resources :users, only: %i[show]
   get "/events/:id(.:format)", to: "events#show"
   get "/components", to: "pages#components"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
