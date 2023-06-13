408456df82ffc2aad165d63cc6c49ae3c5586eed
G <130964102+giacdc@users.noreply.github.com>
2023-06-13 17:38:11 +0200

    working bookings in Event#show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,10 +14,12 @@ class BookingsController < ApplicationController
     end
   end
 
-  def accept
+  def update
     @booking = Booking.find(params[:id])
     @booking.is_accepted = true
+    @booking.save
     authorize @booking
+    redirect_to event_path(@booking.event)
   end
 
   private
