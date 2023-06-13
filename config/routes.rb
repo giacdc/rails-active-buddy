9110e494da27152f08da0b6a5496b4cf446ea997
Nestor Martinez <nesturron@gmail.com>
2023-06-13 19:16:05 +0200

    Started preparing chatrooms implementation

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
 
   get "/events/:id(.:format)", to: "events#show"
   get "/components", to: "pages#components"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :chatrooms, only: :show
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_135126) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_151748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_135126) do
     t.bigint "participant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["creator_id"], name: "index_chatrooms_on_creator_id"
     t.index ["event_id"], name: "index_chatrooms_on_event_id"
     t.index ["participant_id"], name: "index_chatrooms_on_participant_id"
