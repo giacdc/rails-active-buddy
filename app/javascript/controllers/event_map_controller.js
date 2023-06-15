2d216a0cb599d123ac7bb6a5117269ed15a63f52
Adeline de Gaulejac <adelinedegaulejac@gmail.com>
2023-06-16 00:02:34 +0200

    fixed map on show event and top navbar index z

--- app/javascript/controllers/event_map_controller.js
+++ app/javascript/controllers/event_map_controller.js
@@ -11,7 +11,6 @@ export default class extends Controller {
   static targets = ["map"]
 
   connect() {
-console.log('hello :)');
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
@@ -43,6 +42,6 @@ console.log('hello :)');
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 10, maxZoom: 15, duration: 0 })
   }
 }
